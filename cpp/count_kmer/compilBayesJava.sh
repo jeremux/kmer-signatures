b94abddd725788e18a286b641348dd4c14e63433
jfontaine <jfontaine@03fad38e-d51a-4bec-b665-4e7f05cec1f8>
2014-08-25 16:15:23 +0000

    generate graph
    git-svn-id: https://scm.gforge.inria.fr/svn/mitomer/trunk@137 03fad38e-d51a-4bec-b665-4e7f05cec1f8
    
    Former-commit-id: 5fdde7a7732689790bf42bd0c6f99fa7ad3262a2

---
+++ cpp/count_kmer/compilBayesJava.sh
@@ -0,0 +1,1 @@
+javac -classpath $CLASSPATH:../../data_mining/weka-3-6-11/weka.jar:../../data_mining/weka-3-6-11/libsvm.jar ../../data_mining/weka_learn/src/IncrementalClassifier.java -d .
---
+++ cpp/count_kmer/execAndEval.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use Getopt::Long;
+use Cwd 'abs_path';
+
+
+
+my $list;
+my $root;
+my $start;
+my $end;
+my $step;
+my $out;
+my $nCross;
+my $learn;
+my $key="genomes";
+my $kmerPath;
+my $sample = -1;
+
+
+GetOptions (
+			'list=s' => \$list,
+            'root=s' 	 => \$root,
+            'start=i' => \$start,
+            'end=i'	 => \$end,			
+            'step=i' => \$step,
+            'nCross=i' => \$nCross,
+            'key=s' => \$key,
+             'kmer=s' => \$kmerPath,
+             'sample=i' => \$sample);
+
+
+# exmple de ligne de commande:
+
+# ./execAndEval.pl --root "../../create_db/Eukaryota__2759/Alveolata__33630/" --start "50" --end "300" --step "50" --nCross "10" --list list.txt --kmer "pattern.txt" --sample "20"
+
+
+system("sh compilBayesJava.sh;");
+system("make realclean;");
+system("make;");
+$root = abs_path($root);
+
+system("echo \"#learn_length\tpredict_length\ttrue_positive\"") if -e "result.log";
+open(LIST,'<',$list) || die "Can't open $list\n";
+
+while (<LIST>) {
+	my $l = $_;
+	chomp($l);
+
+	print "learn length = $l\n";
+	if($l ne "")
+	{
+
+		
+		print "./count_kmer --learn \"$l\" --sample \"$sample\" --key $key --kmer $kmerPath --root \"$root\" --start \"$start\" --step \"$step\" --end \"$end\";\n";
+		system("./count_kmer --learn \"$l\" --sample \"$sample\" --key \"$key\" --kmer \"$kmerPath\" --root \"$root\" --start \"$start\" --step \"$step\" --end \"$end\";");
+
+		if ($l eq "-1")
+		{
+			$l = "complete";
+		}
+
+		for (my $i = $start; $i <= $end; $i+=$step) 
+		{
+			system("echo  -n \"$l\t$i\t\" >> result.log;");
+			print "sh execCrossVal.sh $root $nCross $i $l >> result.log;\n";
+			system("sh execCrossVal.sh $root $nCross $i $l >> result.log;");
+		}
+	}
+}
+
+system("make realclean;");
